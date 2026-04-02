7726007cf4efd55f5cb3f9dc96963991646fff2c
tumenbaevaj <tumenbaevaj@gmail.com>
2026-04-02 17:28:57 +0600

    add TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,24 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "144.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000; // default 4000
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+}
--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -8,16 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "144.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000; // default 4000
-    }
+public class TextBoxTests extends TestBase {
 
     @Test
     void successfulFillFormTest() {
